28006191949afa03fbc91ddeb02d8eabfc083590
peaman <aminnjfi0@gmail.com>
2023-02-22 20:54:37 +0330

    HW14

---
+++ HW/HW14/2.py
@@ -0,0 +1,13 @@
+# from fastapi import FastAPI, HTTPException
+# import requests
+#
+# app = FastAPI()
+#
+# @app.exception_handlers(HTTPException)
+# async def handler(request, exc):
+#     if exc.status_code == 500:
+#         url = 'https://127.0.0.1'
+
+
+
+
