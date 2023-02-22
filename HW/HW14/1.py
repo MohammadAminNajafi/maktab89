34ad946f6651307302644a5ac8af751aadac4be4
peaman <aminnjfi0@gmail.com>
2023-02-22 19:33:18 +0330

    HW14

---
+++ HW/HW14/1.py
@@ -0,0 +1,14 @@
+from typing import List
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+items = [
+    {'fname': 'Mohammad', 'lname': 'Pedrami'},
+    {'fname': 'MohammadAmin', 'lname': 'Najafi'}
+]
+
+@app.get('/search')
+def search(term: str = Query(None, min_length=1)):
+    results = [item for item in items if term.lower() in item['fname'].lower() or term.lower() in item['lname'].lower()]
+    return results
