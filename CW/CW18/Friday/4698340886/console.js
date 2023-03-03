8fdee5de0ef67a09501619bf405514f216469f40
peaman <aminnjfi0@gmail.com>
2023-03-03 20:37:33 +0330

    CW18

---
+++ CW/CW18/Friday/4698340886/console.js
@@ -0,0 +1,9 @@
+function Geeks1() {
+    document.getElementById("row1").remove();
+}
+function Geeks2() {
+    document.getElementById("row2").remove();
+}
+function Geeks3() {
+    document.getElementById("row3").remove();
+}
